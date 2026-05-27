043449beb54e264353109f6e0306acc61c72e0c9
Mmoka03 <l55922477@gmail.com>
2026-05-27 15:30:05 +0900

    build: env.example 분리 및 compose 빌드 조정 기본 .env로 대충 정의한 것을 example로 재정의 실개발 할때는 .dev/.development 혹은 .prod/.production 으로 구분한다. 자세한 사항은 README.md 참고

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,22 +1,41 @@
-import { defineConfig } from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
-    react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+export default defineConfig(({mode}) => {
+    const env = loadEnv(mode, process.cwd(), '');
 
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
-  assetsInclude: ['**/*.svg', '**/*.csv'],
-})
+    return {
+        server: {
+            host: '0.0.0.0', // 모든 네트워크 인터페이스에서 접근 허용
+            port: Number(env.VITE_PORT) || 5173,
+            hmr: {
+                clientPort: Number(env.VITE_PORT) || 5173, // 브라우저가 접속할 포트값 고정
+            },
+            proxy: {
+                '/api': {
+                    target: env.TRIPE_API_URL || 'http://backend:8080', // 스프링 부트 주소
+                    changeOrigin: true,
+                },
+            },
+            watch: {
+                ignored: ['**/node_modules/**', '**/dist/**', '**/public/**']
+            }
+        },
+        plugins: [
+            // Make를 사용하기 위해 아래 플러그인들이 필요함.
+            react(),
+            tailwindcss(),
+        ],
+        resolve: {
+            alias: {
+                // Alias @ to the src directory
+                '@': path.resolve(__dirname, './src'),
+            },
+        },
+
+        // 원시적인 파일값 가져오기. 절대 .css, .tsx, or .ts 파일 형식을 추가하지 말 것.
+        assetsInclude: ['**/*.svg', '**/*.csv'],
+    }
+});
